c24df57fef5357ed2a57a92b417df4d1402d0230
Michele <50552288+PokeMichele@users.noreply.github.com>
2023-02-06 22:34:02 +0100

    Added the Script Configuration

--- XWrap-Qt.py
+++ XWrap-Qt.py
@@ -1,28 +1,42 @@
 import os
 import time
-import tempfile
+from pathlib import Path
 from PIL import Image, ImageSequence
 
+# Create the TMP_DIR if it doesn't exist
+TMP_DIR = Path(os.environ.get("TMPDIR", "/tmp")) / "xwrap-qt"
+TMP_DIR.mkdir(parents=True, exist_ok=True)
 
+# Extract frames from the GIF file and save them to TMP_DIR
 def extract_frames_from_gif(gif_path):
-    with Image.open(gif_path) as gif:
-        frames = [frame.copy() for frame in ImageSequence.Iterator(gif)]
-
-    temp_folder = tempfile.mkdtemp()
-    frame_paths = []
-    for i, frame in enumerate(frames):
-        frame_path = os.path.join(temp_folder, f'frame_{i}.jpg')
+    gif = Image.open(gif_path)
+    frame_paths = [TMP_DIR / f"frame_{i}.jpeg" for i, frame in enumerate(ImageSequence.Iterator(gif))]
+    for frame, frame_path in zip(ImageSequence.Iterator(gif), frame_paths):
         frame = frame.convert("RGB")
-        frame.save(frame_path)
-        frame_paths.append(frame_path)
-
+        frame.save(frame_path, "JPEG")
     return frame_paths
 
+# Set the wallpaper using pcmanfm-qt
+def set_wallpaper(frame_path):
+    os.system(f"pcmanfm-qt --set-wallpaper {frame_path}")
 
-gif_path = input("Enter the path to the GIF file: ")
-frame_paths = extract_frames_from_gif(gif_path)
+def main(gif_path, sleep_time):
+    frame_paths = extract_frames_from_gif(gif_path)
+    while True:
+        for frame_path in frame_paths:
+            set_wallpaper(frame_path)
+            time.sleep(sleep_time)
 
-while True:
-    for frame_path in frame_paths:
-        os.system(f'pcmanfm-qt --set-wallpaper {frame_path}')
-        time.sleep(1/24)  # You can adjust the Sleep Time to control the Animation Speed
+if __name__ == "__main__":
+    config_path = Path(__file__).parent / "XWrap-Qt.conf"
+    if config_path.exists():
+        with open(config_path) as f:
+            gif_path = f.readline().strip()
+            sleep_time = float(f.readline().strip())
+    else:
+        print("Config File not found, Creating a new one...")
+        gif_path = input("Enter the Path to the GIF File: ")
+        sleep_time = float(input("Enter the Sleep Time (in Seconds) Between Frames: "))
+        with open(config_path, "w") as f:
+            f.write(f"{gif_path}\n{sleep_time}")
+    main(gif_path, sleep_time)
